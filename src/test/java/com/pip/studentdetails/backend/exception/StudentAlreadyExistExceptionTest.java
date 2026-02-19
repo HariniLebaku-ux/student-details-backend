a9e24ed1ff18be6fe929ced3e58498adf4c5594c
Lebaku <harini.lebaku@accenture.com>
2026-02-19 15:08:15 +0530

    exception handler added

---
+++ src/test/java/com/pip/studentdetails/backend/exception/StudentAlreadyExistExceptionTest.java
@@ -0,0 +1,22 @@
+package com.pip.studentdetails.backend.exception;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentAlreadyExistsExceptionTest {
+
+    @Test
+    void studentAlreadyExistExceptionMessage_Success() {
+        String msg = "Student with id ST_090 already exists";
+        StudentAlreadyExistsException ex = new StudentAlreadyExistsException(msg);
+
+        assertEquals(msg, ex.getMessage());
+    }
+
+    @Test
+    void shouldThrowStudentAlreadyExistsException() {
+        assertThrows(StudentAlreadyExistsException.class, () -> {
+            throw new StudentAlreadyExistsException("Student already exists");
+        });
+    }
+}
